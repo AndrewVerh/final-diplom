f980942791fa2d067931469a526fc7fb6926a9c6
Andy Verh <bepx@inbox.ru>
2023-02-28 00:18:24 +0600

    реализация поискового движка

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,13 +1,45 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 public class BooleanSearchEngine implements SearchEngine {
-    //???
+
+    private final Map<String, List<PageEntry>> dataBase = new HashMap<>();
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
         // прочтите тут все pdf и сохраните нужные данные,
         // тк во время поиска сервер не должен уже читать файлы
+        for (File pdf : pdfsDir.listFiles()) {
+            var doc = new PdfDocument(new PdfReader(pdf));
+            int pageNumber = doc.getNumberOfPages();
+            for (int i = 1; i <= pageNumber; i++) {
+                var page = doc.getPage(i);
+                var text = PdfTextExtractor.getTextFromPage(page);
+                var words = text.split("\\P{IsAlphabetic}+");
+                Map<String, Integer> frequences = new HashMap<>(); // мапа, где ключ это слово, а значение - частота
+                for (var word : words) { // перебираем слова
+                    if (word.isEmpty()) {
+                        continue;
+                    }
+                    word = word.toLowerCase();
+                    frequences.put(word, frequences.getOrDefault(word, 0) + 1);
+                }
+                for (String word : frequences.keySet()) {
+                    PageEntry pageEntry = new PageEntry(pdf.getName(), i, frequences.get(word));
+                    if (dataBase.containsKey(word)) {
+                        dataBase.get(word).add(pageEntry);
+                    } else {
+                        dataBase.put(word, new ArrayList<>());
+                        dataBase.get(word).add(pageEntry);
+                    }
+                }
+            }
+        }
+
     }
 
     @Override
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,35 @@
-import java.io.File;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
-        System.out.println(engine.search("бизнес"));
 
-        // здесь создайте сервер, который отвечал бы на нужные запросы
-        // слушать он должен порт 8989
-        // отвечать на запросы /{word} -> возвращённое значение метода search(word) в JSON-формате
+    public static void main(String[] args) {
+        try (ServerSocket server = new ServerSocket(8989)) { // старт сервера
+            BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
+
+
+            while (true) { // в цикле принимаем подключения
+
+                try (
+                        Socket socket = server.accept();
+                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        PrintWriter out = new PrintWriter(socket.getOutputStream())
+                ) {
+                    String request = in.readLine();
+                    String answer = engine.search(request).toString();
+                    out.println(answer);
+
+                }
+
+            }
+
+        } catch (IOException e) {
+            System.out.println("Не могу стартовать сервер");
+            e.printStackTrace();
+        }
+
     }
+
 }
